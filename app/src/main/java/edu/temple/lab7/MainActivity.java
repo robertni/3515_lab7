fbac673f88d903364b901cf3e3a8f8688eb51936
Robert Ni <robertni@outlook.fr>
2021-03-23 14:08:17 -0400

    populated book list with books

--- app/src/main/java/edu/temple/lab7/MainActivity.java
+++ app/src/main/java/edu/temple/lab7/MainActivity.java
@@ -3,12 +3,27 @@ package edu.temple.lab7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    BookList bookList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bookList = new BookList();
+
+        for (int i = 0; i < Arrays.asList(getResources().getStringArray(R.array.book_titles)).size(); i++) {
+            String title = Arrays.asList(getResources().getStringArray(R.array.book_titles)).get(i);
+            String author = Arrays.asList(getResources().getStringArray(R.array.book_authors)).get(i);
+            Book book = new Book(title, author);
+            bookList.addBook(book);
+        }
     }
 }
